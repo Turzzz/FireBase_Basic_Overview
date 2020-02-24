71c17acb9d8c5fd85794d90e604a5d20499f2b42
Sakib Hossain <sakibhossain321@gmail.com>
2020-02-25 02:21:11 +0600

    Sent data by 2 way (By Object,By Hashmap)

--- app/src/main/java/com/example/firebasebasicoverview/MainActivity.java
+++ app/src/main/java/com/example/firebasebasicoverview/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        //sent data by object
+        UserData userData = new UserData("Karim","29");
+        databaseReference.setValue(userData);
+
+//        //sent data by hashmap
+//        HashMap<String,String> hashMap = new HashMap<>();
+//        hashMap.put("Team","Brazil");
+//        hashMap.put("Club","Barcelona");
+//        databaseReference.setValue(hashMap);
+
     }
 }
